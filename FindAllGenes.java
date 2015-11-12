ea38b35a2e08be9228d7b4a32d19828e52c987f9
Greg Whorley <greg@whorley.com>
2015-11-12 13:16:12 -0800

    new changes to FindAllGenes.java

--- FindAllGenes.java
+++ FindAllGenes.java
@@ -8,48 +8,62 @@
  */
 public class FindAllGenes {
     public int findStopIndex(String dna, int index) {
-        //find the first occurrence of each stop codon to the right of index
-        //from those stop codons that are a multiple of three from index, return the smallest index position
-        //return -1 if no stop codon was found
-        
-        // 3. If the length of the substring between ATG and any of these three stop codons is a multiple of three, then a candidate for a gene
-        //    is the start codon through the end of the stop codon
-        // 4. If there is more than one valid candidate, the smallest such string is the gene. The gene includes the start and stop codon
-        while (true) {
-            if (dna.contains("tag") && dna.indexOf("tag",index) % 3 == 0) {
-                //do something
-            }
-            else if (dna.contains("tga") && dna.indexOf("tga",index) % 3 == 0) {
-                //do something
-            }
-            else if (dna.contains("taa") && dna.indexOf("taa",index) % 3 == 0) {
-                //do something
-            }
-            else {
-                return -1;
-            }
+        //Write the method findStopIndex that has two parameters dna and index, where dna is a String of 
+        //DNA and index is a position in the string. This method finds the first occurrence of each stop 
+        //codon to the right of index. From those stop codons that are a multiple of three from index, it 
+        //returns the smallest index position. It should return -1 if no stop codon was found and there 
+        //is no such position. This method was discussed in one of the videos.
+        int stop1 = dna.indexOf("tag", index);
+        if (stop1 == -1 || (stop1 - index) % 3 != 0) {
+            stop1 = dna.length();
+        }
+        int stop2 = dna.indexOf("tga", index);
+        if (stop2 == -1 || (stop2 - index) % 3 != 0) {
+            stop2 = dna.length();
+        }
+        int stop3 = dna.indexOf("taa", index);
+        if (stop3 == -1 || (stop3 - index) % 3 != 0) {
+            stop3 = dna.length();
+        }
+        if (stop1 == dna.length() && stop2 == dna.length() && stop3 == dna.length()) {
+            return -1;
+        }
+        else {
+            return Math.min(stop1, Math.min(stop2, stop3));
         }
     }
     public void printAll(String dna) {
         //this method should print all the genes it finds in DNA
         //look repeatedly for a gene; if a gene is found, then print it and continue looking for another gene
         //this method should call findStopIndex
+        System.out.println("DNA string is:");
+        System.out.println(dna);
+        System.out.println("Gene(s) found:");
         
-        // 5. If no start codon was found, then you are done.
-        String dnaLower = dna.toLowerCase();
-        if (dnaLower.indexOf("atg") == -1) {
-            System.out.println("Start codon not found! Exiting");
-            System.exit(0);
+        int marker = 0;
+        while (marker < dna.length()) {
+            int startCodon = dna.indexOf("atg", marker);
+            if (startCodon == -1) {
+                break;
+            }
+            int stopCodon = findStopIndex(dna.substring(marker),marker);
+            if (stopCodon == -1) {
+                break;
+            }
+            else {
+                System.out.println(dna.substring(startCodon,stopCodon+3));
+                marker = stopCodon+3;
+            }
         }
-        // 1. to find the first gene, find the start codon ATG
-        int startCodon = dnaLower.indexOf("atg");
-        // 2. Next look immediately past ATG for the first occurrence of each of the three stop codons TAG, TGA, and TAA
-        int stopCodon = findStopIndex(dnaLower, startCodon);
-        // 6. If a start codon was found, but no gene was found, then start searching for another gene via the next occurrence of a start
-        //    codon starting immediately after the start codon that didn't yield a gene.
         
     }
     public void testFinder() {
         //test method that prints full DNA strings set within this method and prints genes found in the strings
+        String dna1 = "ATGAAATGAAAA";
+        String dna2 = "ccatgccctaataaatgtctgtaatgtaga";
+        String dna3 = "CATGTAATAGATGAATGACTGATAGATATGCTTGTATGCTATGAAAATGTGAAATGACCCA";
+        printAll(dna1);
+        printAll(dna2);
+        printAll(dna3);
     }
 }
