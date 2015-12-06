07e5165c3fdd1c7a01ad400ea98b813e2bcf3ad6
Greg Whorley <greg@whorley.com>
2015-12-06 14:30:52 -0800

    Added test data and finished assignment 1: word lengths

--- CommonWords.java
+++ CommonWords.java
@@ -39,9 +39,9 @@ public class CommonWords
 		}
 	}
 	void countShakespeare(){
-		//String[] plays = {"caesar.txt", "errors.txt", "hamlet.txt",
-			//          "likeit.txt", "macbeth.txt", "romeo.txt"};
-	    String[] plays = {"small.txt"};
+		String[] plays = {"caesar.txt", "errors.txt", "hamlet.txt",
+			          "likeit.txt", "macbeth.txt", "romeo.txt"};
+	    //String[] plays = {"small.txt"};
 		String[] common = getCommon();
 		int[] counts = new int[common.length];
 		for(int k=0; k < plays.length; k++){
